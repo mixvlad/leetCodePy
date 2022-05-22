5fb2905cc9fb5b71ccfc737994dab4a7da0bc4b3
mixvlad <mixa@xakep.ru>
2022-05-22 12:43:04 +0300

    PalindromicSubstrings

---
+++ PalindromicSubstrings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        L, r = len(s), 0
+        for i in range(L):
+            for a,b in [(i,i),(i,i+1)]:
+                while a >= 0 and b < L and s[a] == s[b]: a -= 1; b += 1
+                r += (b-a)//2
+        return r
+
+    def test(self):
+        assert self.countSubstrings("abc") == 3
+
+if __name__ == "__main__":
+    Solution().test()
+    print("Everything passed")
